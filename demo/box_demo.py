d264f6a8d4ae7e6f2a2dbefc5ab58fc3252f6cbb
liujianfu <liujf@baojia.com>
2020-01-18 19:33:35 +0800

    新建项目 第一次提交

--- demo/box_demo.py
+++ demo/box_demo.py
@@ -4,6 +4,7 @@
 @author:Administrator
 @file: box_demo.py
 @time: 2020-01-18 下午 4:30
+https://segmentfault.com/a/1190000015926584   异常值检测
 '''
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -25,14 +26,12 @@ def programmer_1(file_name):
     y.sort();
     print(x)
     print(y)
+    #以下参数都是经过调试的，需要具体问题具体调试
     for i in range(len(x)):
-        # 处理临界情况， i=0时
+        # 处理临界情况， i》0时
         if i>0:
-           # plt.annotate(y[i],xy=(x[i],y[i]),xytext=(x[i]+0.05-0.8/(y[i]-y[i-1]),y[i]));
             plt.annotate(y[i],xy=(x[i],y[i]),xytext=(x[i] +0.05-0.8 / (y[i] - y[i - 1]), y[i]))
-
         else:
-            #plt.annotate(y[i],xy=(x[i],y[i]),xytext=(x[i]+0.08,y[i]))
             plt.annotate(y[i],xy=(x[i], y[i]), xytext=(x[i] + 0.08, y[i]))
     plt.show()
 path="../data/catering_sale.xls";
